b494b8e47124d3e0e3d6eedf55e9cdc182822e47
DESKTOP-TDTE37I\윤지성 <njds2045@gmail.com>
2022-01-25 16:27:26 +0900

    16. 복습하기(추상클래스와 템플릿 메서드 활용 - GameLevel)

--- Chapter3/src/ch16/gamelevel/MainBoardPlay.java
+++ Chapter3/src/ch16/gamelevel/MainBoardPlay.java
@@ -1,4 +1,10 @@
 package ch16.gamelevel;
+/* Player가 있고 Player는 GameLevel속성을 가집니다. 각 GameLevel단계마다 run(), jump(), turn() 세 가지 기능이 업그레이드 됩니다.
+  1. 초보자 레벨 : 천천히 달립니다. run()만 가능.
+  2. 중급자 레벨 : 빠르게 달리고, 점프할 수 있습니다. run(), jump()가능.
+  3. 고급자 레벨 : 엄청 빠르게 달리고, 높게 점프하고, 턴 할 수 있습니다. run(), jump(), turn() 가능
+  - Player는 한번에 하나의 레벨 상태만을 가질 수 있습니다.
+  - Player가 play()중에 레벨에 있는 go(int count)라는 메서드를 호출하면 run()하고 count횟수만큼 jump()하고 turn()합니다. */
 
 public class MainBoardPlay {
 
